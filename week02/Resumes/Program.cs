4b7ec5cd8748d9d05814c7115147a758c2273e7c
HarryMichael <harrymichaelasa@gmail.com>
2025-07-14 07:32:06 +0000

    Resume

--- week02/Resumes/Program.cs
+++ week02/Resumes/Program.cs
@@ -4,6 +4,17 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Resumes Project.");
+        // Create jobs using constructor
+        Job job1 = new Job("Software Developer", "Microsoft", 2019, 2022);
+        Job job2 = new Job("Manager", "Amazon", 2022, 2023);
+
+        // Create resume and assign jobs
+        Resume resume = new Resume();
+        resume._name = "Michael Asamoah";
+        resume._jobs.Add(job1);
+        resume._jobs.Add(job2);
+
+        // Display the resume information
+        resume.Display();
     }
 }
